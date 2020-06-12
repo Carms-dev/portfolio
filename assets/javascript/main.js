6309e12996269b3ec2d1e7ea1465106756e53775
Carms Ng <carmenhyng89@gmail.com>
2020-06-12 13:46:28 -0400

    nav event listeners added

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,13 +2,33 @@ const menuBtn = document.querySelector('.menu-btn');
 const menuList = document.querySelector('#menu-list');
 let menuOpen = false;
 
+const openNav = () => {
+    menuBtn.classList.add('open-menu');
+    menuList.style.display = "flex";
+    menuOpen = true;
+}
+
+const closeNav = () => {
+    menuBtn.classList.remove('open-menu');
+    menuList.style.display = "none";
+    menuOpen = false;
+}
+
+
 menuBtn.addEventListener('click', () => {
     if (!menuOpen) {
-        menuBtn.classList.add('open-menu');
-        menuList.style.display = "flex";
+        openNav();
+
+        window.addEventListener('keydown', (e) => {
+            if (e.keyCode === 27) {
+                closeNav();
+            }
+        })
+
+        menuList.addEventListener('click', () => {
+            closeNav();
+        })
     } else {
-        menuBtn.classList.remove('open-menu');
-        menuList.style.display = "none";
+        closeNav();
     }
-    menuOpen = !menuOpen;
 })
